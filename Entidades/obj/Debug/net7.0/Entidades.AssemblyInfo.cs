00759bc5ec17f41a6850a674540e19bd1e5fa6f7
Aguu21 <agustinpineda04@gmail.com>
2024-05-21 16:38:10 -0300

    Eliminar el diagrama de clases

--- Entidades/obj/Debug/net7.0/Entidades.AssemblyInfo.cs
+++ Entidades/obj/Debug/net7.0/Entidades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34c2315ef6e09d30957581e426daf444b5202155")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c72f371cc6e25c03f26dbc7aceb95d65d7214a3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
