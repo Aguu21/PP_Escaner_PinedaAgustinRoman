a4394f3b0dfbfd5efeaf3a4f414fcdf7eb2bee86
Aguu21 <agustinpineda04@gmail.com>
2024-06-05 15:21:59 -0300

    Modificar el main para que solo este lo necesario

--- Test/Test.cs
+++ Test/Test.cs
@@ -9,24 +9,10 @@ namespace Test
             // LIBROS
             // Caminos felices
             Libro l1 = new Libro("Yerma", "García Lorca, Federico", 1995, "11111", "22222", 100);
-            Libro l2 = new Libro("Bodas de sangre", "García Lorca, Federico", 1997, "11112", "22223", 200);
-            // Barcode repetido
-            Libro l3 = new Libro("Codebar repetido", "García Lorca, Federico", 2003, "11113", "22222", 3);
-            // ISBN repetido
-            Libro l4 = new Libro("ISBN repetido", "García Lorca, Federico", 2003, "11112", "22224", 2);
-            // Título-autor repetido
-            Libro l5 = new Libro("Yerma", "García Lorca, Federico", 2003, "55555", "66666", 1);
 
             //MAPAS 
             // Caminos felices
             Mapa m1 = new Mapa("Buenos Aires", "Instituto Geográfico de Buenos Aires", 2005, "", "99999", 30, 15); //450
-            Mapa m2 = new Mapa("Mendoza", "Instituto Geográfico de Mendoza", 2008, "", "99990", 100, 30); //300
-            Mapa m3 = new Mapa("Santa Fe", "Instituto Geográfico de Santa Fe", 2010, "", "99991", 80, 30); //2400
-            Mapa m4 = new Mapa("Corrientes", "Instituto Geográfico de Corrientes", 2013, "", "99992", 50, 25); //1250
-            // Barcode repetido
-            Mapa m5 = new Mapa("Barcode repetido", "Instituto Geográfico", 2015, "", "99999", 40, 15);//600
-            // Título - autor - superficie
-            Mapa m6 = new Mapa("Buenos Aires", "Instituto Geográfico de Buenos Aires", 2005, "", "99993", 30, 15);//200
 
             //ESCANERS
             Escaner l = new Escaner("HP", Escaner.TipoDoc.libro);
